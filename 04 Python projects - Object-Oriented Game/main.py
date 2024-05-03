239cce6999b6270193b08d0ffe011fdf1b78b843
Zivan R <135060460+Zivan-R@users.noreply.github.com>
2024-05-04 00:34:18 +0200

    Did objects and room classes

--- 04 Python projects - Object-Oriented Game/main.py
+++ 04 Python projects - Object-Oriented Game/main.py
@@ -10,3 +10,48 @@ class GameObject:
         self.appearance = appearance
         self.feel = feel
         self.smell = smell
+
+    def look(self):
+        return f"You look at {self.name}. {self.appearance}\n"
+
+    def touch(self):
+        return f"You touch the {self.name}. {self.feel}\n"
+
+    def sniff(self):
+        return f"You sniff the {self.name}. {self.smell}\n"
+    
+game_object = GameObject("Knife", "Pointy", "Cutty cutty", "Smeels like bacon")
+
+print(game_object.sniff())
+
+class Room:
+    escape_code = 0
+    game_objects = []
+
+    def __init__(self, esc_code, game_obj):
+        self.escape_code = esc_code
+        # self.code_tries = 0
+        self.game_objects = game_obj
+    
+    # def try_code(self):
+    #     code_try = input("What is the code?: \n")
+        
+    #     if code_try == self.escape_code:
+    #         print("You found the right code! You win")
+    #         return
+    #     else:
+    #         self.code_tries += 1
+    #         if self.code_tries > 3:
+    #             print("More than 3 errors. Unfortunately you lose")
+    #             return
+
+    def check_code(self, code):
+        return self.escape_code == code
+    
+    def get_game_object_names(self):
+        names = []
+        for object in self.game_objects:
+            names.append(object.name)
+        return names
+
+    
