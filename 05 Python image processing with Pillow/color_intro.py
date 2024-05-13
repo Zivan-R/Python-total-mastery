f10077730c624fa8979253773554ddbf27cdcf9d
Zivan R <135060460+Zivan-R@users.noreply.github.com>
2024-05-13 22:01:25 +0200

    Finished color intro

--- 05 Python image processing with Pillow/color_intro.py
+++ 05 Python image processing with Pillow/color_intro.py
@@ -1,6 +1,22 @@
 from PIL import Image
 
-image = Image.open('cat3.jpg')
+image = Image.open('checkerboard.png')
 
 # get one pixel
-print(image.getpixel((0, 0)))
+# print(image.getpixel((0, 0)))
+
+# grayscale images (Puts only one value to the 2 others)
+# red_grayscale_image = image.getchannel('R')
+# blue_grayscale_image = image.getchannel('B')
+
+# change pixel color (pos, color)
+# image.putpixel((0,0), (255,0,0))
+
+# Exercise: change all black pixels to red on checkerboard
+
+for x in range(image.size[0]):
+    for y in range(image.size[1]):
+        if image.getpixel((x, y))[0] == 0:
+            image.putpixel((x, y), (200, 20, 20))
+
+image.show()
